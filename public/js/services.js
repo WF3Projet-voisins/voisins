3d9129756ef5fe9da97a30c0df5674cf496b9ff6
frederic <vazvieirafrederic@outlook.fr>
2020-05-20 15:30:13 +0200

    Mise a jour du filtre

--- public/js/services.js
+++ public/js/services.js
@@ -3,11 +3,9 @@ $(document).ready(function(){
 
     $("#myChoice").on("keyup", function() {
       var value = $(this).val().toLowerCase();
-      $("p").filter(function() {
+      $("#cardService").filter(function() {
         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
       });
     });
-
-
 
   });
