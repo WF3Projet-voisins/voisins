71bfb996aac099521b75d4873b62211a71430adb
SamDbs <dbs.samantha@gmail.com>
2020-05-23 19:07:29 +0200

    FormInscription setup ranking et si adresse mail déjà dans bdd

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -29,98 +29,84 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 class UserController extends AbstractController
 {
-    
+
     public function addUserAction(Request $request, UserPasswordEncoderInterface $passwordEncoder, RankingRepository $rankingRepository, CategoryRepository $categoryRepository, SubCategoryRepository $subCategoryRepository, UserRepository $userRepository)
     {
-        
+
         $users = $userRepository->findAll();
         $ranking = $rankingRepository->findAll();
         $category = $categoryRepository->findAll();
 
-        $subCategory = $subCategoryRepository->find('3');
-        
+        $subCategory = $subCategoryRepository->find('23');
+
         $form = null;
         // 1) build the form
         $newUser = new User();
         $form = $this->createForm(UserType::class, $newUser);
         // 2) handle the submit (will only happen on POST)
         $form->handleRequest($request);
-       
+
         if ($form->isSubmitted()) {
-            foreach($users as $user){ 
-                if ($newUser->getEmail() === $user->getEmail() ){
-                   
-                    $this->addFlash('danger', 'L\'adresse mail existe déjà!');
-                    return $this->redirectToRoute('userAdd');
-                }                
-                else {
-                    foreach($ranking as $rank){
-                        if ($rank->getName() === "Newbie" ){
-                        $newUser->setRanking($rank);
-                       }
-                    
-                    $newUser->setTimeGauge('0');
-                    $newUser->setTotalTimeServiceGiven('0');
-                    $newUser->setRoles(['ROLE_USER']);
+            $user = $userRepository->findOneBy(['email' => $newUser->getEmail()]);
+
+            if ($user) {
+
+                dump($newUser->getEmail());
+                $this->addFlash('danger', 'L\'adresse mail existe déjà!');
+                  return $this->redirectToRoute('userAdd'); 
+            } else {
+                $newbie = $rankingRepository->findOneBy(['name' => 'Newbie']);
+                $newUser->setRanking($newbie);
 
 
-
-                    foreach($category as $cat){
-                        if ($cat->getName() === "Cuisine" ){
-                      $newUser->addCategoryAffinity($cat);
-                        }
-                    
-
-                   // $newUser->addCategoryAffinity($category);
+                $newUser->setTimeGauge('0');
+                $newUser->setTotalTimeServiceGiven('0');
+                $newUser->setRoles(['ROLE_USER']);
 
 
-                    $newUser->addSubCatAffinity($subCategory);
-
-                    $file = $form->get('image')->getData();
-                    $newUser = $form->getData();
-                        if ($file) {
-                            $newFilename = FormsManager::handleFileUpload($file, $this->getParameter('uploads'));
-                            $newUser->setImage($newFilename);
-                        } else {
-                            $newUser->setImage('https://cdn.pixabay.com/photo/2020/05/03/13/09/puppy-5124947_960_720.jpg');
-                        }
-            // 3) Encode the password (you could also do this via Doctrine listener)
-                    $password = $passwordEncoder->encodePassword($newUser, $newUser->getPlainPassword());
-                    $newUser->setPassword($password);
-
-           
-            // 4) save the User!
-                    $entityManager = $this->getDoctrine()->getManager();
-                    $entityManager->persist($newUser);
-                    $entityManager->flush();
+                $file = $form->get('image')->getData();
+                $newUser = $form->getData();
+                if ($file) {
+                    $newFilename = FormsManager::handleFileUpload($file, $this->getParameter('uploads'));
+                    $newUser->setImage($newFilename);
+                } else {
+                    $newUser->setImage('https://cdn.pixabay.com/photo/2020/05/03/13/09/puppy-5124947_960_720.jpg');
+                }
+                // 3) Encode the password (you could also do this via Doctrine listener)
+                $password = $passwordEncoder->encodePassword($newUser, $newUser->getPlainPassword());
+                $newUser->setPassword($password);
 
 
-                    $token = new UsernamePasswordToken(
+                // 4) save the User!
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($newUser);
+                $entityManager->flush();
+
+
+                $token = new UsernamePasswordToken(
                     $newUser,
                     $password,
                     'main',
                     $newUser->getRoles()
-                    );
+                );
 
-                    $this->get('security.token_storage')->setToken($token);
-                    $this->get('session')->set('_security_main', serialize($token));
+                $this->get('security.token_storage')->setToken($token);
+                $this->get('session')->set('_security_main', serialize($token));
 
-                    return $this->redirectToRoute('chooseTypeServices', ['id'=> $newUser->getId()]);
-                
-                }}
+                return $this->redirectToRoute('chooseTypeServices', ['id' => $newUser->getId()]);
             }
         }
-        }
 
 
-        foreach($users as $user){
+
+
+
+        foreach ($users as $user) {
             return $this->render(
                 'user/formInscription.html.twig',
-                array('form' => $form->createView(),'user' => $user, 'content' => $ranking)
+                array('form' => $form->createView(), 'user' => $user, 'content' => $ranking)
             );
         }
-
-     
     }
 
     public function getUserAction(Request $request, UserRepository $userRepository, $id, CategoryRepository $categoryRepository, SubCategoryRepository $subCategoryRepository)
@@ -131,8 +117,8 @@ class UserController extends AbstractController
 
         $formUser = $this->createForm('App\Form\UserType', $user);
         $formUser->handleRequest($request);
-       
-        if ($formUser->isSubmitted() ) {
+
+        if ($formUser->isSubmitted()) {
             $user = $formUser->getData();
             $file = $formUser->get('image')->getData();
             if ($file) {
@@ -148,7 +134,7 @@ class UserController extends AbstractController
             return $this->redirectToRoute('userProfile', ['id' => $id]);
         }
 
-        
+
         return $this->render('user/profileUser.html.twig', ["user" => $user, "categories" => $categories, "subCategories" => $subCategories, "formUser" => $formUser->createView()]);
     }
 
