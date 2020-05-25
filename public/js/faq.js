137f5de77b88104d9070820a960dbe633dcdd935
SamDbs <dbs.samantha@gmail.com>
2020-05-26 00:14:20 +0200

    Generation Q/A FAQ de la BDD

---
+++ public/js/faq.js
@@ -0,0 +1,4 @@
+
+function collapse($id){
+    $('.collapse').collapse()
+}
--- src/Controller/FaqsController.php
+++ src/Controller/FaqsController.php
@@ -2,23 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\FaqRepository;
 use App\Repository\UserRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class FaqsController extends AbstractController
 {
     /**
      * @Route("/faqs", name="faqs")
      */
-    public function index(UserRepository $userRepository)
+    public function index(TokenStorageInterface $token, FaqRepository $faqRepository, UserRepository $userRepository)
     {
-        $users = $userRepository->findAll();
-        foreach($users as $user){
+        $user=$token->getToken()->getUser();
+        $faqs = $faqRepository->findAll();
 
         return $this->render('faqs/index.html.twig', [
-            'controller_name' => 'FaqsController', 'user' => $user
+            'controller_name' => 'FaqsController', 'user' => $user, "faqs"=>$faqs
         ]);
-    }
+    
     }
 }
