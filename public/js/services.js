7d5d7e43a5ed068bc3e55dcc92dd174d9b852480
frederic <vazvieirafrederic@outlook.fr>
2020-05-26 15:07:43 +0200

    mise a jour

--- public/js/services.js
+++ public/js/services.js
@@ -5,7 +5,7 @@ var route = "";
 function myfunction(event){
  
   route =  event ;
-  console.log(event);
+ 
  
   var x = document.getElementById(event);
     if (x.style.display === "none") {
@@ -19,23 +19,25 @@ function myfunction(event){
 
 
 
-
 var message = document.getElementById('message');
 var messageDiv = document.getElementsByClassName('message');
 
 
-console.log(messageDiv);
+
 
 
 $(document).ready(function(){
-
 
 
       $( "select" ).change(function() {
         $( "select option:selected" ).each(function() {
           var value = $('#myChoice').val();
           $('.card').hide();
+       
+
           $("div[name=" + value + "]").show();
+
+          
         });
       })
 })
